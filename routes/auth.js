514007eb0801c37df199a24250f39a14a39498cb
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2021-08-05 17:30:29 +0530

    fix: use github app

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 // Initiate GitHub authentication
 router.get('/auth/github', (_, res) => {
-    return res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+    return res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_APP_CLIENT_ID}&redirect_uri=http://localhost:3000/auth/github/callback`);
 });
 
 // GitHub auth callback
@@ -12,8 +12,8 @@ router.get('/auth/github/callback', async (req, res) => {
     const code = req.query.code;
 
     axios.post('https://github.com/login/oauth/access_token', {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        client_id: process.env.GITHUB_APP_CLIENT_ID,
+        client_secret: process.env.GITHUB_APP_CLIENT_SECRET,
         code: code
     }).then(async (response) => {
         const accessToken = response.data.split('&')[0].split('=')[1];
